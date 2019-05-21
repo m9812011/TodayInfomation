5ef48b36a20ee247a2978a50532079144d7e84b5
melvin <m9812011@gmail.com>
2019-05-21 19:46:27 +0800

    1.RadioButton 動畫切換 2.未使用ButterKnife套件

---
+++ app/src/main/java/melvinlin/com/todayinfomation/BaseActivity.java
@@ -0,0 +1,36 @@
+package melvinlin.com.todayinfomation;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+//import butterknife.ButterKnife;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        ButterKnife.bind(this);
+        ViewInject annotation = this.getClass().getAnnotation(ViewInject.class);
+
+//        if (annotation != null) {
+//            int mainLayoutId = annotation.mainlayout();
+//            if (annotation != null) {
+//                if (mainLayoutId > 0) {
+//                    setContentView(mainLayoutId);
+//                } else {
+//                    throw new RuntimeException("mainlayoutId < 0");
+//                }
+//            } else {
+//                throw new RuntimeException("mainlayoutId = null");
+//            }
+//        }
+        int mainLayoutId = R.layout.activity_main;
+        if (mainLayoutId > 0) {
+            setContentView(mainLayoutId);
+        } else {
+            throw new RuntimeException("mainLayoutId < 0");
+        }
+    }
+}
