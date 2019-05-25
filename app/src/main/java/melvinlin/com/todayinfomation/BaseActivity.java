16b8e7327b49fd4fceda37223ee4179c2f822034
melvin <m9812011@gmail.com>
2019-05-25 19:17:18 +0800

    執行 Butterknife 不衝突

--- app/src/main/java/melvinlin/com/todayinfomation/BaseActivity.java
+++ app/src/main/java/melvinlin/com/todayinfomation/BaseActivity.java
@@ -4,14 +4,14 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-//import butterknife.ButterKnife;
+import butterknife.ButterKnife;
 
 public class BaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        ButterKnife.bind(this);
+        ButterKnife.bind(this);
         ViewInject annotation = this.getClass().getAnnotation(ViewInject.class);
 
 //        if (annotation != null) {
--- app/src/main/java/melvinlin/com/todayinfomation/MainActivity.java
+++ app/src/main/java/melvinlin/com/todayinfomation/MainActivity.java
@@ -1,13 +1,27 @@
 package melvinlin.com.todayinfomation;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.fac_main_home)
+    FloatingActionButton actionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    public void onClick(View view) {
+        Log.d("actionButton", "actionButton");
     }
 }
