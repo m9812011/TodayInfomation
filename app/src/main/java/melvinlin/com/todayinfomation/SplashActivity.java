65b82e7d621a8cfd694f2457aa743a288413ca0e
melvin <m9812011@gmail.com>
2019-05-26 10:29:27 +0800

    - SplashActivity 優化代碼

--- app/src/main/java/melvinlin/com/todayinfomation/SplashActivity.java
+++ app/src/main/java/melvinlin/com/todayinfomation/SplashActivity.java
@@ -11,43 +11,30 @@ import android.widget.VideoView;
 
 import java.io.File;
 
-public class SplashActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
-    private FullScreenVideoView mVideoView;
-    private TextView mTvTimer;
+@ViewInject(mainLayoutId = R.layout.activity_splash)
+public class SplashActivity extends BaseActivity {
+
+    @BindView(R.id.vv_play)
+    FullScreenVideoView mVideoView;
+    @BindView(R.id.tv_splash_timer)
+    TextView mTvTimer;
+
     private CustomCountDownTimer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
 
-        mVideoView = (FullScreenVideoView)findViewById(R.id.vv_play);
-        mTvTimer = (TextView)findViewById(R.id.tv_splash_timer);
-        mTvTimer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //顯式啟動
-                startActivity(new Intent(SplashActivity.this, MainActivity.class));
-            }
-        });
-        Uri uri = Uri.parse("android.resource://" + getPackageName() + File.separator + R.raw.splash);
-        mVideoView.setVideoURI(uri);
-        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mp) {
-                mp.start();
-            }
-        });
+        initListener();
+        initVideo();
+        // 把初始化Timer及相關內容抽出到Presenter層中
+        initTimer();
+    }
 
-
-        mVideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-            @Override
-            public void onCompletion(MediaPlayer mp) {
-                mp.start();
-            }
-        });
-
+    private void initTimer() {
         timer = new CustomCountDownTimer(5, new CustomCountDownTimer.ICountDownHandler() {
             @Override
             public void onTicker(int time) {
@@ -61,6 +48,35 @@ public class SplashActivity extends AppCompatActivity {
         });
 
         timer.start();
+    }
+
+    private void initVideo() {
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + File.separator + R.raw.splash);
+        mVideoView.setVideoURI(uri);
+        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.start();
+            }
+        });
+
+
+    }
+
+    private void initListener() {
+        mTvTimer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //顯式啟動
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            }
+        });
+        mVideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.start();
+            }
+        });
     }
 
     @Override
